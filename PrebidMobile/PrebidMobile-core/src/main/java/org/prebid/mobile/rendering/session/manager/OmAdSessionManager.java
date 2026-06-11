a5e4c31051b5faad76f37f530a31e23a82fba9aa
isaac <isaac@delightroom.com>
2026-06-11 16:32:59 +0900

    프리비드 안드로이드 OMSDK 업데이트

--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/session/manager/OmAdSessionManager.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/session/manager/OmAdSessionManager.java
@@ -23,22 +23,22 @@ import android.webkit.WebView;
 
 import androidx.annotation.Nullable;
 
-import com.iab.omid.library.prebidorg.Omid;
-import com.iab.omid.library.prebidorg.ScriptInjector;
-import com.iab.omid.library.prebidorg.adsession.AdEvents;
-import com.iab.omid.library.prebidorg.adsession.AdSession;
-import com.iab.omid.library.prebidorg.adsession.AdSessionConfiguration;
-import com.iab.omid.library.prebidorg.adsession.AdSessionContext;
-import com.iab.omid.library.prebidorg.adsession.CreativeType;
-import com.iab.omid.library.prebidorg.adsession.FriendlyObstructionPurpose;
-import com.iab.omid.library.prebidorg.adsession.ImpressionType;
-import com.iab.omid.library.prebidorg.adsession.Owner;
-import com.iab.omid.library.prebidorg.adsession.Partner;
-import com.iab.omid.library.prebidorg.adsession.VerificationScriptResource;
-import com.iab.omid.library.prebidorg.adsession.media.InteractionType;
-import com.iab.omid.library.prebidorg.adsession.media.MediaEvents;
-import com.iab.omid.library.prebidorg.adsession.media.Position;
-import com.iab.omid.library.prebidorg.adsession.media.VastProperties;
+import com.iab.omid.library.delightroom.Omid;
+import com.iab.omid.library.delightroom.ScriptInjector;
+import com.iab.omid.library.delightroom.adsession.AdEvents;
+import com.iab.omid.library.delightroom.adsession.AdSession;
+import com.iab.omid.library.delightroom.adsession.AdSessionConfiguration;
+import com.iab.omid.library.delightroom.adsession.AdSessionContext;
+import com.iab.omid.library.delightroom.adsession.CreativeType;
+import com.iab.omid.library.delightroom.adsession.FriendlyObstructionPurpose;
+import com.iab.omid.library.delightroom.adsession.ImpressionType;
+import com.iab.omid.library.delightroom.adsession.Owner;
+import com.iab.omid.library.delightroom.adsession.Partner;
+import com.iab.omid.library.delightroom.adsession.VerificationScriptResource;
+import com.iab.omid.library.delightroom.adsession.media.InteractionType;
+import com.iab.omid.library.delightroom.adsession.media.MediaEvents;
+import com.iab.omid.library.delightroom.adsession.media.Position;
+import com.iab.omid.library.delightroom.adsession.media.VastProperties;
 
 import org.prebid.mobile.LogUtil;
 import org.prebid.mobile.TargetingParams;
--- PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/session/manager/OmModelMapper.java
+++ PrebidMobile/PrebidMobile-core/src/main/java/org/prebid/mobile/rendering/session/manager/OmModelMapper.java
@@ -16,8 +16,8 @@
 
 package org.prebid.mobile.rendering.session.manager;
 
-import com.iab.omid.library.prebidorg.adsession.FriendlyObstructionPurpose;
-import com.iab.omid.library.prebidorg.adsession.media.PlayerState;
+import com.iab.omid.library.delightroom.adsession.FriendlyObstructionPurpose;
+import com.iab.omid.library.delightroom.adsession.media.PlayerState;
 import org.prebid.mobile.rendering.models.internal.InternalFriendlyObstruction;
 import org.prebid.mobile.rendering.models.internal.InternalPlayerState;
 
